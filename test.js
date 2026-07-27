02977878e5128c4705d75c65bc7b04224eed10ac
Skycader <62342371+Skycader@users.noreply.github.com>
2026-07-27 11:30:00 +0300

    OK

--- test.js
+++
@@ -1,5 +0,0 @@
-const f = (txt) => {
-  const r = /^\s*<html>(?:(?!<\/?html)[\s\S]*)<\/html>\s*$/; //не работает
-  const r = /^\s*<html>(?:(?!<\/?html>)[\s\S])*<\/html>\s*$/; //работает
-  return r.test(txt);
-};
